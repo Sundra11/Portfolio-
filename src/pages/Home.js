f4965607266445385819f25fd1057d1328addeb8
sundra <sundrabalakrishnan@gmail.com>
2023-01-29 01:48:09 +0800

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import EmailIcon from "@mui/icons-material/Email";
